eccb61bc0fe83c5255a8dd489deb8889fc1b5678
dragonbegin <juan.reyes.mba@gmail.com>
2019-06-05 18:44:40 -0500

    Todo App completed

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -2,4 +2,17 @@
 // feel free to change this component.js into TodoList.js
 
 import React from "react";
+import Todo from "./Todo";
 
+function TodoList(props) {
+  console.log('TodoList.js props',props)
+  return(
+      <div>
+          {props.tasks.map(task => (
+            <Todo {...task} key={task.id} />
+          ))}
+          
+      </div>
+  );
+}
+export default TodoList;
